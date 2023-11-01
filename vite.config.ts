4f49960dddeb16d3b86d15527e61fd15b208ff38
bogdanpryvalov <pryvalov.bogddan@gmail.com>
2023-11-01 14:57:34 +0200

    added openssh certificate to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
 
 import react from '@vitejs/plugin-react';
 
@@ -10,6 +11,12 @@ export default defineConfig({
       '@i18n': path.resolve(__dirname, './i18n'),
       src: path.resolve(__dirname, './src'),
       '@jestUtils': path.resolve(__dirname, './src/jestUtils'),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-privkey.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost-cert.pem')),
     },
   },
 });
